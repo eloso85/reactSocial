1d4c157918e0215119047c110d3656e2f0e00eef
unknown <eloso85@gmail.com>
2020-03-28 21:28:46 -0500

    added unlike and like route

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const app = require('express')();
 
 const FBAuth = require('./util/fbAuth')
 
-const {getAllScreams, postOneScream,getScream,commentOnScream} = require("./handlers/screams")
+const {getAllScreams, postOneScream,getScream,commentOnScream,likeScream,unlikeScream} = require("./handlers/screams")
 const {signUp,login, uploadImage, addUserDetails,getAuthenticatedUser} = require('./handlers/users')
 
 //Scream routes
@@ -13,6 +13,9 @@ app.get('/screams', getAllScreams);
 app.post('/scream', FBAuth, postOneScream);
 app.get('/scream/:screamId', getScream);
 app.post('/scream/:screamId/comment',FBAuth, commentOnScream)
+app.get('/scream/:screamId/like',FBAuth,likeScream)
+app.get('/scream/:screamId/unlike',FBAuth,unlikeScream)
+
 
 
 
